ccb6bbb932f1498b29ee9608658323abc8062f93
daviedavie100 <innocentdavie@yahoo.com>
2025-04-09 10:21:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -196,6 +196,52 @@
       }
     );
   };
+
+
+
+
+
+  // Typed Initiate
+  // Typed.js Initialization
+  if ($('#daviefolio-hero .typed-text').length) {
+    var typed_strings = $('#daviefolio-hero .typed-text')
+      .text()
+      .split(', ');
+
+    var typed = new Typed('#daviefolio-hero h2', {  // Target the empty <h2>
+      strings: typed_strings,
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true
+    });
+  }
+  /*
+    if ($('#daviefolio-hero .flexslider .slider-text .slider-text-inner .typed-text').length) {
+      var typed_strings = $('#daviefolio-hero .flexslider .slider-text .slider-text-inner .typed-text')
+        .text()
+        .split(', ');
+  
+      var typed = new Typed('#daviefolio-hero .flexslider .slider-text .slider-text-inner h2', {
+        strings: typed_strings,
+        typeSpeed: 100,
+        backSpeed: 20,
+        smartBackspace: false,
+        loop: true
+      });
+    }*/
+  /*
+    if ($('#daviefolio-hero .flexslider .slider-text .slider-text-inner h2').length == 1) {
+      var typed_strings = $('#daviefolio-hero .flexslider .slider-text .slider-text-inner .typed-text').text();
+      var typed = new Typed('#daviefolio-hero .flexslider .slider-text .slider-text-inner h2', {
+        strings: typed_strings.split(', '),
+        typeSpeed: 100,
+        backSpeed: 20,
+        smartBackspace: false,
+        loop: true
+      });
+    }
+  */
   /*
     var sliderMain = function () {
       $("#daviefolio-hero .flexslider").flexslider({
